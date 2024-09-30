2ce3a4791d3a38925a8d8fd1a705dae3f4e484d1
Osman Buğra BOLAT <123267351+osmanbugrabolat@users.noreply.github.com>
2024-10-01 02:48:42 +0300

    data structures

---
+++ data_structures.py
@@ -0,0 +1,40 @@
+# --> DATA STRUCTURES <-- #
+
+
+x = 1 #(int)
+print(type(x))
+
+y = 1.1 #(float)
+print(type(y))
+
+z = 2j + 1 #(complex)
+print(type(z))
+
+k = "hello" #(string)
+print(type(k))
+
+l = True #(boolean)
+print(type(l))
+
+m = ["a" , "b" , "c"] #(list)
+print(type(m))
+
+n = {"name":"bugra", "age": 25, "degree": "university"} #(dictionary)
+print(type(n))
+
+o = ("a", "b", "c") #(tuple)
+print(type(o))
+
+p = {"a", "b", "c"} #(set)
+print(type(p))
+
+
+
+#String Slices
+
+exp = "Hello My AI World!"
+print(exp[3])
+print(exp[0:4])
+
+
+print("My" in exp)
